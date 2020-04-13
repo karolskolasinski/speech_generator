5d2a8c7ff4e16265b989d23bb5c4e1d40d3e0e6a
Karol Skolasiński <karolskolasinski@gmail.com>
2020-04-13 19:53:01 +0200

    js: segments

---
+++ js/generator.js
@@ -0,0 +1,51 @@
+let firstSegment = [
+    "Koleżanki i koledzy,",
+    "Z drugiej strony,",
+    "Podobnie,",
+    "Nie zapominajmy jednak o tym że,",
+    "W ten sposób,",
+    "Praktyka dnia codziennego dowodzi że,",
+    "Wagi i znaczenia tych problemów nie trzeba szerzej uzasadniać,",
+    "Różnorakie i bogate doświadczenia,",
+    "Troska organizacji a szczególnie,",
+    "Wyższe założenia ideowe, a także",
+    "Szanowni Państwo,",
+    "Założenia naszej misji, a także"
+];
+
+var secondSegment = [" realizacja nakreślonych zadań programowych",
+    " zakres i miejsce szkolenia kadr",
+    " stały wzrost ilości i zakresu naszej aktywności",
+    " aktualna struktura organizacji",
+    " nowy model działalności organizacyjnej",
+    " dalszy rozwój róznych form działalności",
+    " stałe zabepieczenie informacyjno-promocyjne naszej działalności",
+    " wzmacnianie i rozwijanie struktur",
+    " konsultacje z szerokim aktywem",
+    " rozpoczęcie powszechnej akcji kształtowania postaw",
+];
+
+let thirdSegment = [" zmusza nas do przeanalizowania",
+    " spełnia istotną rolę w kształtowaniu",
+    " wymaga sprecyzowania i określenia",
+    " pomaga w przygotowaniu i realizacji",
+    " zabezpiecza udział szerokiej grupie w kształtowaniu",
+    " spełnia ważne zadania w wypracowaniu",
+    " uniemożliwia w większym stopniu tworzenie",
+    " powoduje docenienie wagi",
+    " przedstawia interesującą próbę sprawdzenia",
+    " pociąga za sobą proces wdrażania i unowocześniania",
+];
+let fourthSegment = [
+    " istniejących warunków administracyjno-finansowych. ",
+    " dalszych kierunków rozwoju. ",
+    " systemu powszechnego uczestnictwa. ",
+    " postaw uczestników wobec zadań stawianych przez organizację. ",
+    " nowych propozycji. ",
+    " kierunków postępowego wychowania. ",
+    " systemu szkolenia kadr odpowiadającego potrzebom. ",
+    " odpowiednich warunków aktywizacji. ",
+    " modelu rozwoju. ",
+    " form oddziaływania. ",
+    " kierunków postępu prac. ",
+];
